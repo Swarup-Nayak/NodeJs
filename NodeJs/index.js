5df110968313489650b9ec3f70d84731da2840d0
Swarup-Nayak <swarupkumarnayak1995@gmail.com>
2021-11-18 12:46:25 +0530

    added index.js in which middleware file is implemented with routes

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -1,33 +1,31 @@
 const express = require("express");
 const app = express();
+const reqFilter = require("./middleware");
+const route = express.Router();
 
-app.get("", (req, res) => {
-  //   console.log(req.query.name);
-  res.send(`Welcome <h1>${req.query.name} </h1> , this is Home Page
-  <a href="/about">Go to About Page</a>
-  `);
+// app.use(reqFilter);
+
+route.use(reqFilter);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Home Page");
+});
+
+route.get("/users", (req, res) => {
+  res.send("Welcome to Users Page");
+});
+
+route.get("/details", (req, res) => {
+  res.send("Welcome to User Details Page");
+});
+
+route.get("/contact", (req, res) => {
+  res.send("Welcome to Users Contact Page");
 });
 
 app.get("/about", (req, res) => {
-  res.send(`This is About Page
-  <h3>Mobile Numer<h3>
-  <input type="text" placeholder="User Name" value='${req.query.name}'/>
-  <button>Click Me </button>
-  <a href="/">Go to Home Page</a>
-  `);
+  res.send("Welcome to About Page");
 });
 
-app.get("/help", (req, res) => {
-  res.send([
-    {
-      name: "anil",
-      email: "anil@test.com",
-    },
-    {
-      name: "Kapoor",
-      email: "kp@test.com",
-    },
-  ]);
-});
-
+app.use("/", route);
 app.listen(5000);
